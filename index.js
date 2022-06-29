806db098658144ea90bf5a26db72831a6e0a9981
Pavel-Loban <loban_pasha@mail.ru>
2022-06-29 19:37:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const userRouter = require('./routes/user.routes')
 const postRouter = require('./routes/post.routes')
+const cors = require('corse')
 
 const PORT = process.env.PORT || 8080
 
 const app = express()
+
+const corsOptions = {
+origin: "*"
+}
+
+app.use(corse(corseOptions))
+
 app.use(express.json())
 
 app.use('/api', userRouter)
